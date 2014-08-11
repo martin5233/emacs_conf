48d9621dd8aa7d2a24d5e86606b298ebef697c9c
apel <Martin.Apel@SIMPACK.de>
2014-08-11 12:05:48 +0200

    Delete trailing whitespace upon save

--- init.el
+++ init.el
@@ -512,6 +512,9 @@
 (add-hook 'shell-mode-hook
           'dirtrack-mode)
 
+(add-hook 'before-save-hook
+          'delete-trailing-whitespace)
+
 (add-hook 'after-save-hook
   'executable-make-buffer-file-executable-if-script-p)
 
