41426f4eec68316bf548a9808eda3567c89326be
mal1 <Martin.APEL@3ds.com>
2022-05-30 14:22:32 +0200

    Set up calendar for work

--- init-calendar.el
+++ init-calendar.el
@@ -1,11 +1,13 @@
 (setq diary-file "~/.emacs.d/diary")
-(setq calendar-url "https://nextcloud.apel-boecker.de:32671/nextcloud/remote.php/dav/calendars/martin/familie?export")
+(setq calendar-url "http://localhost:1080/users/Martin.APEL@3ds.com/calendar/")
 (setq calendar-view-diary-initially-flag t)
 (setq diary-number-of-entries 3)
 (setq calendar-time-display-form '(24-hours ":" minutes))
 (setq calendar-week-start-day 1)
 (setq appt-display-diary t)
 (setq org-agenda-include-diary t)
+
+(defvar ma--getcal-last-update nil "Last time the calendar has been updated.")
 
 (defun ma--getcal-do (url file)
   "Download ics file and add it to file"
@@ -19,10 +21,15 @@
   (message (concat "Loading " calendar-url " into " diary-file))
   (with-current-buffer (find-file-noselect diary-file)
     (erase-buffer) ;; to avoid duplicating events
-    (ma--getcal-do calendar-url diary-file)))
+    (ma--getcal-do calendar-url diary-file)
+    (basic-save-buffer)))
 
+(defun ma--getcal-if-necessary ()
+  "Reload the calendar if it hasn't been updated for an hour."
+  (when (or (not (and (floatp ma--getcal-last-update) (< (- (float-time) ma--getcal-last-update) 3600))))
+    (ma-getcal)))
 
 (appt-activate 1)
 
 (ma-getcal)
-(run-with-timer 3600 t 'ma-getcal)
+(run-with-idle-timer 60 t 'ma--getcal-if-necessary)
