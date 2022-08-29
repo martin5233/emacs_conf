053e4c0c1a8a16f57efa8cc28ebdec39d94079d1
mal1 <Martin.APEL@3ds.com>
2022-08-29 15:29:05 +0200

    Marginalia for Jira, WIP

--- ma-funcs/marginalia-jira.el
+++ ma-funcs/marginalia-jira.el
@@ -1,7 +1,11 @@
 (require 'jiralib)
 (require 'org-jira)
+(require 'savehist)
 
 (defvar ma-jira-key-to-summary-alist nil)
+(defvar ma-jira-issues-to-retrieve nil)
+
+(add-to-list 'savehist-additional-variables 'ma-jira-issues-to-retrieve)
 
 (defun ma-marginalia-jira--fill-cache()
   "Transform the response from the JIRA server to fill the cache."
@@ -10,18 +14,33 @@
          (keys (cl-mapcar 'org-jira-get-issue-key issues))
          (summaries (cl-mapcar 'org-jira-get-issue-summary issues)))
 
+    (setq ma-jira-issues-to-retrieve (remove-duplicates (append ma-jira-issues-to-retrieve keys) :test 'string=))
     (setq ma-jira-key-to-summary-alist (cl-pairlis keys summaries))))
 
 (defun ma-marginalia-jira--annotator (cand)
-  (message "ma-jira-annotator called with " cand)
   (let ((summary (cdr (assoc cand ma-jira-key-to-summary-alist))))
     (if summary
         (marginalia--fields (summary :truncate 1.0 :face 'marginalia-value))
       (marginalia--fields ("Unknown" :truncate 1.0 :face 'marginalia-value)))))
 
+;; (defun ma-marginalia-jira--completing-read-advice(prompt collection &optional predicate
+;;                                                          require-match initial-input
+;;                                                          hist def inherit-input-method)
+;;   (message "Advice called")
+;;   (when (arrayp collection)
+;;     (mapcar (lambda(entry)
+;;               (when (stringp entry)
+;;                 (message (concat "Found entry " entry))))
+;;             collection))
+;;   )
+
+
+
 (eval-after-load 'marginalia
   (lambda ()
+;;    (advice-add #'completing-read-default :before #'ma-marginalia-jira--completing-read-advice)
     (add-to-list 'marginalia-prompt-categories '("JIRA" . jira))
+    (add-to-list 'marginalia-command-categories '(org-roam-node-find . jira))
     (add-to-list 'marginalia-annotator-registry
                  '(jira ma-marginalia-jira--annotator))))
 
